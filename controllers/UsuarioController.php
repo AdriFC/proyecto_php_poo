bdae996ed7b9e9e2bba103da5752f0389ad63b2c
AdriFC <adri86vigo@gmail.com>
2021-05-03 13:15:29 +0200

    Creada funcionalidad de login

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -43,6 +43,49 @@ class usuarioController{
         header("Location:".base_url.'usuario/registro');
     }
 
-}
+    public function login(){
+        if(isset($_POST)){
+            //Identificar al usuario
+            //Consulta a la bbdd
+            $usuario = new Usuario;
+            $usuario->setEmail ($_POST['email']);
+            $usuario->setPassword ($_POST['password']);
+
+            $identity = $usuario->login();
+
+            if($identity && is_object($identity)){
+                $_SESSION['identity'] = $identity;
+
+                
+                if($identity->rol == 'admin'){
+                    $_SESSION['admin'] = true;
+                }
+
+                
+            }else{
+                $_SESSION['error_login'] = "Identificación fallida";
+            }
+
+            // var_dump($identity);
+            // die();
+
+            //Crear una sesión
+        }
+        header("Location:".base_url);
+    }
+
+    public function logout(){
+        if(isset($_SESSION['identity'])){
+            unset($_SESSION['identity']);
+        }
+
+        if(isset($_SESSION['admin'])){
+            unset($_SESSION['admin']);
+        }
+
+        header("Location:".base_url);
+    }
+
+}//Fin clase
 
 ?>
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,18 +1,32 @@
 <!-- BARRA LATERAL -->
 <aside id="lateral">
-                <h3>Entrar a la web</h3>
+                
                 <div id="login" class="block_aside">
-                    <form action="#" method="post">
+
+                <?php if(!isset($_SESSION['identity'])): ?>
+                    <h3>Entrar a la web</h3>
+                    <form action="<?=base_url?>usuario/login" method="post">
                         <label for="email">Email</label>
                         <input type="email" name="email">
                         <label for="password">Contaseña</label>
                         <input type="password" name="password">
                         <input type="submit" value="Enviar">
                     </form>
+                <?php else: ?>
+                    <h3><?=$_SESSION['identity']->nombre?> <?=$_SESSION['identity']->apellidos?></h3>
+                <?php endif; ?> 
+
                     <ul>
-                        <li><a href="#">Mis pedidos</a></li>
-                        <li><a href="#">Gestionar pedidos</a></li>
-                        <li><a href="#">Gestionar categorías</a></li>
+                        <?php if(isset($_SESSION['admin'])) : ?>
+                            <li><a href="#">Gestionar categorías</a></li>
+                            <li><a href="#">Gestionar productos</a></li>
+                            <li><a href="#">Gestionar pedidos</a></li>
+                        <?php endif; ?>
+
+                        <?php if(isset($_SESSION['identity'])) : ?>
+                            <li><a href="#">Mis pedidos</a></li>
+                            <li><a href="<?=base_url?>usuario/logout">Cerrar sesión</a></li>
+                        <?php endif; ?>
                     </ul>
                 </div>
             </aside>
