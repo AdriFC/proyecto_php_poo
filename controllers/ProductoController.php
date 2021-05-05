f905ca264595df2858454b6e538b5592072ab1d7
AdriFC <adri86vigo@gmail.com>
2021-05-05 17:35:23 +0200

    Sección 70: Gestión de productos y formulario creación productos.

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -1,10 +1,40 @@
 <?php
 
+require_once 'models/producto.php';
+
 class productoController{
 
     public function index(){
         //Renderizar vista
         require_once 'views/producto/destacados.php';
+    }
+
+    public function gestion(){
+
+        Utils :: isAdmin();
+
+        $producto = new Producto();
+        $productos = $producto->getAll();
+
+        //Cargar vista
+        require_once 'views/producto/gestion.php';
+    }
+
+    public function crear(){
+
+        Utils :: isAdmin();
+
+        //Cargar vista
+        require_once 'views/producto/crear.php';
+    }
+
+    public function save(){
+        
+        Utils :: isAdmin();
+        
+        if(isset($_POST)){
+            var_dump($_POST);
+        }
     }
 
 }
