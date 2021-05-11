0bffe405d2db107a3bfbce269f27564e37968359
AdriFC <adri86vigo@gmail.com>
2021-05-11 14:17:56 +0200

    Sección 72: Carrito de la compra *error al mostrar los productos

--- views/categoria/ver.php
+++ views/categoria/ver.php
@@ -12,13 +12,15 @@
 
             <div class="product">
 
-                <?php if ($product->imagen != null) : ?>
-                    <img src="<?= base_url ?>uploads/images/<?= $product->imagen ?>">
-                <?php else : ?>
-                    <img src="<?= base_url ?>assets/img/camiseta.png">
-                <?php endif ?>
+                <a href="<?= base_url ?>producto/ver&id=<?= $product->id ?>">
+                    <?php if ($product->imagen != null) : ?>
+                        <img src="<?= base_url ?>uploads/images/<?= $product->imagen ?>">
+                    <?php else : ?>
+                        <img src="<?= base_url ?>assets/img/camiseta.png">
+                    <?php endif ?>
+                    <h2><?= $product->nombre ?></h2>
+                </a>
 
-                <h2><?= $product->nombre ?></h2>
                 <p><?= $product->precio ?></p>
                 <a href="" class="button">Comprar</a>
 
--- views/producto/destacados.php
+++ views/producto/destacados.php
@@ -1,20 +1,21 @@
 <h1>Algunos de nuestros productos</h1>
 
-<?php while($product = $productos->fetch_object()) : ?>
+<?php while ($product = $productos->fetch_object()) : ?>
 
-<div class="product">
+    <div class="product">
 
-    <?php if($product->imagen != null) : ?>
-        <img src="<?=base_url?>uploads/images/<?=$product->imagen?>">
-    <?php else: ?>
-        <img src="<?=base_url?>assets/img/camiseta.png">
-    <?php endif ?>
+        <a href="<?=base_url?>producto/ver&id=<?=$product->id?>">
+            <?php if ($product->imagen != null) : ?>
+                <img src="<?= base_url ?>uploads/images/<?= $product->imagen ?>">
+            <?php else : ?>
+                <img src="<?= base_url ?>assets/img/camiseta.png">
+            <?php endif ?>
+            
+        </a>
+        
+        <p><?= $product->precio ?></p>
+        <a href="" class="button">Comprar</a>
 
-    <h2><?= $product->nombre?></h2>
-    <p><?= $product->precio?></p>
-    <a href="" class="button">Comprar</a>
-
-</div>
+    </div>
 
 <?php endwhile; ?>
-
